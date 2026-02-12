d5e922b746cd718097d118f13868789bd3d02cf4
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-02-12 15:48:44 -0300

    ADD: Banco de dados criado.

--- migrate/estrutura_banco.sql
+++ migrate/estrutura_banco.sql
@@ -1,24 +1,20 @@
-CREATE TABLE genero (
- categoria VARCHAR(30) NOT NULL,
+CREATE DATABASE IF NOT EXISTS vibetune;
+USE vibetune;
+
+CREATE TABLE IF NOT EXISTS genero (
+ categoria VARCHAR(30) NOT NULL PRIMARY KEY,
  icone VARCHAR(100),
  cor CHAR(10)
 );
 
-ALTER TABLE genero ADD CONSTRAINT PK_genero PRIMARY KEY (categoria);
-
-
-CREATE TABLE musica (
- codigo_musica CHAR(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS musica (
+ codigo_musica INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  cantor VARCHAR(50),
- duracao TIME(10),
+ duracao TIME,
  titulo VARCHAR(100),
  imagem VARCHAR(300),
- categoria VARCHAR(30)
+ categoria VARCHAR(30),
+ CONSTRAINT FK_musica_genero
+ FOREIGN KEY (categoria)
+ REFERENCES genero (categoria)
 );
-
-ALTER TABLE musica ADD CONSTRAINT PK_musica PRIMARY KEY (codigo_musica);
-
-
-ALTER TABLE musica ADD CONSTRAINT FK_musica_0 FOREIGN KEY (categoria) REFERENCES genero (categoria);
-
-
