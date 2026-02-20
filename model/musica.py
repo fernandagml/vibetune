408fd5aed3073608a398e16be7a118ebf102b4f7
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-02-20 11:21:29 -0300

    ADD: Criação da função salvar_musica.

--- model/musica.py
+++ model/musica.py
@@ -1,8 +1,22 @@
 from database.conexao import conectar
 
 def recuperar_musicas():
+    """Função criada para buscar as músicas salvas no banco de dados."""
     conexao, cursor = conectar()
     cursor.execute("SELECT codigo_musica, cantor, duracao, titulo, imagem, categoria FROM musica;") #Executando a consulta
     musicas = cursor.fetchall() #Recuperando os dados
     conexao.close()
     return musicas
+
+def salvar_musica(cantor:str, duracao:str, titulo:str, imagem:str, categoria:str) -> bool:
+    """Função criada para adicionar uma música no banco de dados."""]
+
+    try:
+        conexao, cursor = conectar()
+        cursor.execute("INSERT INTO musica (cantor, duracao, titulo, imagem, categoria) VALUES (%s, %s, %s, %s, %s);", (cantor, duracao, titulo, imagem, categoria))
+        conexao.commit()
+        conexao.close()
+        return True
+    
+    except:
+        return False
