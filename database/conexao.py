52335547b8e3503c4d09e6ea11bf082c120335f6
Fernanda Gaspar <fernandamarquesluizgaspar@gmail.com>
2026-03-27 15:56:42 -0300

    Add files via upload

--- database/conexao.py
+++ database/conexao.py
@@ -1,14 +1,24 @@
 import mysql.connector as sql
 
 def conectar():
+    tipo_conexao = 'NUVEM'
     #Conectando no banco de dados
-    conec = sql.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="root",
-        database="vibetune"
-    )
+    if tipo_conexao == 'LOCAL':
+        conec = sql.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            password="root",
+            database="vibetune"
+        )
+    else:
+        conec = sql.connect(
+            host="servidor-vibetune-fernanda-web-server-fernanda-vibetune.a.aivencloud.com",
+            port=16941,
+            user="avnadmin",
+            password="AVNS_RZ_rsqrnYIWX1aFuu9I",
+            database="vibetune"
+        )
 
     #Criando o cursor
     cursor = conec.cursor(dictionary=True)
